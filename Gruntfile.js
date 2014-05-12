f885a7435dcd4a26df639072df75c6344f210c86
Fedor Indutny <fedor@indutny.com>
2014-05-12 17:43:39 +0400

    initial

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    watch: {
+      files: [ 'lib/**/*.js' ],
+      tasks: [ 'browserify', 'uglify' ]
+    },
+    browserify: {
+      bg: {
+        src: [ 'lib/background/**/*.js' ],
+        dest: 'chrome/dist/bg.js'
+      },
+      wnd: {
+        src: [ 'lib/app/**/*.js' ],
+        dest: 'chrome/dist/app.js'
+      }
+    },
+    uglify: {
+      mangle: {
+      },
+      compress: {
+        dead_code: true
+      },
+      build: {
+        files: [{
+          src: 'chrome/dist/bg.js',
+          dest: 'chrome/dist/bg.min.js'
+        }, {
+          src: 'chrome/dist/app.js',
+          dest: 'chrome/dist/app.min.js'
+        }]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', [ 'watch', 'browserify', 'uglify' ]);
+};
